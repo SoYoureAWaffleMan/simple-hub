a8fc96bf2dd60fdd4fec651a2b4898019f4951a3
Xander <xander.king@ents24.com>
2020-04-21 19:37:54 +0100

    Build config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  publicPath: '/sov-hub/'
+  publicPath: '/sov-hub/',
+  configureWebpack: {
+    optimization: {
+      splitChunks: false
+    }
+  },
+  filenameHashing: false,
+  pages: {
+    sovApp: {
+      entry: 'src/main.js',
+      filename: 'index.html',
+      minify : false
+    }
+  }
 }
